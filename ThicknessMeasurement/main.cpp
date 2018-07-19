21707f054e6249e006b2877fe83200194afa11f0
Nisheeth Pandey <pandey.nisheeth59916031@gmail.com>
2018-07-19 14:45:11 +0530

    Tossed the progression bar idea. Display slows down the process significantly.

--- ThicknessMeasurement/main.cpp
+++ ThicknessMeasurement/main.cpp
@@ -7,10 +7,17 @@ void makeGrayScale(sf::Image& base, sf::Image& gray, sf::RenderWindow& window);
 
 using uint = unsigned int;
 
+sf::Font font;
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 100), "Percent done");
+	//sf::RenderWindow window(sf::VideoMode(510, 125), "Percent done");
 	
+	if (!font.loadFromFile("C:\\Windows\\Fonts\\Calibri.ttf"))
+	{
+		std::cout << "Font not loaded";
+		return -1;
+	}
 
 	std::string baseImagePath = "G:\\TEST_IMAGES\\Original\\";
 	std::string imageName;
@@ -27,7 +34,7 @@ int main()
 
 	sf::Vector2u baseImageSize = baseImage.getSize();
 
-	makeGrayScale(baseImage,grayScaleImage);
+	makeGrayScale(baseImage,grayScaleImage, window);
 
 	std::string grayImagePath = "G:\\TEST_IMAGES\\Grayscale\\" + imageName;
 	grayScaleImage.saveToFile(grayImagePath);
@@ -38,11 +45,16 @@ int main()
 void makeGrayScale(sf::Image& base, sf::Image& gray, sf::RenderWindow& window)
 {
 	sf::Vector2u baseImageSize = base.getSize();
+	sf::RectangleShape barBack(sf::Vector2f(500,50));
+	barBack.setPosition(5,5);
+	barBack.setFillColor(sf::Color::White);
+
 
 	gray.create(baseImageSize.x, baseImageSize.y, sf::Color::White);
 
 	uint imageSize = baseImageSize.x * baseImageSize.y;
 	double percentCompleted = 0.f;
+
 	for (uint i = 0; i < baseImageSize.x; i++)
 	{
 		for (uint j = 0; j < baseImageSize.y; j++)
@@ -52,10 +64,20 @@ void makeGrayScale(sf::Image& base, sf::Image& gray, sf::RenderWindow& window)
 			sf::Color newPix = { grayCol, grayCol, grayCol};
 
 			gray.setPixel(i, j, newPix);
-			percentCompleted = static_cast<double>(i * baseImageSize.x + j);
+		
+			/*percentCompleted = static_cast<double>(i * baseImageSize.x + j);
 			percentCompleted /= imageSize;
+			std::string percStr = std::to_string(percentCompleted) + "%";
+			sf::String str(percStr);
+
+			sf::Text percs(str, font, 25);
+			percs.setPosition(105,10);
+			percs.setFillColor(sf::Color::Cyan);
 
 			window.clear(sf::Color::Black);
+			window.draw(barBack);
+			window.draw(percs);
+			window.display();*/
 		}
 	}
 }
